cf28b3cdbe2da926e14f2e545645e389d56373bf
kammeph <ph.kammerer@googlemail.com>
2022-10-10 21:36:07 +0200

    feat: adjusted styling on schools page and added dialogs

--- src/pages/Schools.tsx
+++ src/pages/Schools.tsx
@@ -1,114 +1,89 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faPlus, faSave, faCancel, faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faPlus, faPen } from '@fortawesome/free-solid-svg-icons';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import useSchoolsApi from '../api/schools';
-import { button, card, headers, input } from '../styles';
+import useSchoolsApi, { School } from '../api/schools';
+import SchoolEditDialog from '../components/SchoolEditDialog';
+import DeleteDialog from '../components/DeleteDialog';
+import { Role, User } from '../api/users';
+import RequireAuth from '../components/RequireAuth';
 
 const SCHOOLS = 'schools';
 
-const Schools = () => {
-  const [addSchoolName, setAddSchoolName] = useState('');
-  const [editSchoolId, setEditSchoolId] = useState<string | undefined>();
-  const [editSchoolName, setEditSchoolName] = useState<string | undefined>();
-  const editSchoolNameRef = useRef<HTMLInputElement>(null);
-
-  const { getSchools, addSchool, updateSchool, deleteSchool } = useSchoolsApi();
+const Schools: React.FC<{ currentUser?: User }> = ({ currentUser }) => {
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+  const [deleteId, setDeleteId] = useState('');
+  const [editSchool, setEditSchool] = useState<School>({ name: '' });
+  const { getSchools, deleteSchool } = useSchoolsApi();
   const queryClient = useQueryClient();
   const { data } = useQuery([SCHOOLS], getSchools);
-
-  const addSchoolMutation = useMutation(
-    async (e: any) => {
-      e.preventDefault();
-      addSchool(addSchoolName);
-      setAddSchoolName('');
-    },
-    {
-      onSuccess: () => queryClient.invalidateQueries([SCHOOLS])
-    }
-  );
-
-  const updateSchoolMutation = useMutation(
-    async (e: any) => {
-      e.preventDefault();
-      if (editSchoolId && editSchoolName) updateSchool({ id: editSchoolId, name: editSchoolName });
-      handleEditMode();
-    },
-    {
-      onSuccess: () => queryClient.invalidateQueries([SCHOOLS])
-    }
-  );
 
   const deleteSchoolMutation = useMutation(deleteSchool, {
     onSuccess: () => queryClient.invalidateQueries([SCHOOLS])
   });
 
-  const handleEditMode = (id?: string, name?: string) => {
-    setEditSchoolId(id);
-    setEditSchoolName(name);
-  };
-
-  useEffect(() => {
-    if (!!editSchoolId) editSchoolNameRef.current?.focus();
-  }, [editSchoolId]);
-
   return (
-    <div className="w-full h-full p-5 overflow-auto">
-      <h1 className={headers.h1}>Schulen</h1>
-      <p>{data?.error}</p>
-      <form className="flex gap-1" onSubmit={addSchoolMutation.mutate}>
-        <input
-          className={`${input.basic} grow`}
-          type="text"
-          placeholder="School name"
-          value={addSchoolName}
-          onChange={e => setAddSchoolName(e.target.value)}
-        />
-        <button className={button.fab}>
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-      </form>
-      {data?.schools?.map(school => (
-        <div key={school.id} className={`${card} my-2`}>
-          <div className="flex flex-row items-center justify-between">
-            {editSchoolId !== school.id && (
-              <>
-                <p>{school.name}</p>
-                <div className="grid grid-flow-col gap-1">
-                  <button className={button.fab} onClick={() => handleEditMode(school.id, school.name)}>
-                    <FontAwesomeIcon icon={faPen} />
-                  </button>
-                  <button className={button.fab} onClick={() => deleteSchoolMutation.mutate(school.id)}>
-                    <FontAwesomeIcon icon={faTrash} />
-                  </button>
-                </div>
-              </>
-            )}
-          </div>
-          {editSchoolId === school.id && (
-            <form className="flex flex-row items-center justify-between" onSubmit={updateSchoolMutation.mutate}>
-              {editSchoolId === school.id && (
-                <input
-                  className={input.underlined}
-                  ref={editSchoolNameRef}
-                  type="text"
-                  value={editSchoolName}
-                  onChange={e => setEditSchoolName(e.target.value)}
-                />
-              )}
+    <>
+      <SchoolEditDialog isOpen={editDialogOpen} school={editSchool} onClose={() => setEditDialogOpen(false)} />
+      <DeleteDialog
+        isOpen={deleteDialogOpen}
+        title="Delete school"
+        message="Are you sure that you want to delete the selected school?"
+        onDelete={() => {
+          deleteSchoolMutation.mutate(deleteId);
+          setDeleteDialogOpen(false);
+        }}
+        onClose={() => setDeleteDialogOpen(false)}
+      />
+      <div className="w-full h-full p-5 overflow-auto">
+        <div className="flex justify-between items-center mb-4 px-2">
+          <h1 className="h1">Schulen</h1>
+          <RequireAuth
+            userRoles={currentUser?.roles}
+            allowedRoles={[Role.SysAdmin]}
+            element={
+              <button
+                className="btn-fab"
+                onClick={() => {
+                  setEditSchool({ name: '' });
+                  setEditDialogOpen(true);
+                }}
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+            }
+          />
+        </div>
+        {data?.schools?.map(school => (
+          <div key={school.id} className="card my-2">
+            <div className="flex flex-row items-center justify-between">
+              <p className="font-semibold font-accent">{school.name}</p>
               <div className="grid grid-flow-col gap-1">
-                <button className={button.fab} type="submit">
-                  <FontAwesomeIcon icon={faSave} />
+                <button
+                  className="btn-fab"
+                  onClick={() => {
+                    setEditSchool(school);
+                    setEditDialogOpen(true);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPen} />
                 </button>
-                <button className={button.fab} type="button" onClick={() => handleEditMode()}>
-                  <FontAwesomeIcon icon={faCancel} />
+                <button
+                  className="btn-fab"
+                  onClick={() => {
+                    school?.id && setDeleteId(school.id);
+                    setDeleteDialogOpen(true);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
                 </button>
               </div>
-            </form>
-          )}
-        </div>
-      ))}
-    </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
