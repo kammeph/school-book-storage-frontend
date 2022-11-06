13d728c925a4bbeba4b655234a6a5683d8919a06
kammeph <ph.kammerer@googlemail.com>
2022-11-06 22:57:05 +0100

    feat: added pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'School Book Storage',
+        short_name: 'Books',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        lang: 'de',
+        scope: '/',
+        icons: [
+          {
+            src: '/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        theme_color: '#4f46e5'
+      }
+    })
+  ]
+});
